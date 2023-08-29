dd7d690ef32c12ccc9c83168adf73ee4d8bd4e17
schahrazede <schahrazedemohamedvall@gmail.com>
2023-08-29 12:16:14 +0200

    cache fichier images et .env

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/UserController");
 const emailRegex = require("../middleware/emailRegex");
-router.post("/signup", emailRegex, userController.signup);
+router.post("/signup",emailRegex, userController.signup);
 router.post("/login", userController.login);
 module.exports = router;
